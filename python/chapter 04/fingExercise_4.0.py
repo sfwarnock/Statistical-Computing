7ba4cecd8ed0ff26bcc3dfc6bec4ea0eb2477d42
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-08-28 06:06:58 -0600

    Add files via upload

---
+++ python/chapter 04/fingExercise_4.0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 28 2018
+
+@author: Scott Warnock
+"""
+
+# finger exercise 4.0
+#
+# Wrtie a function isIn that accepts two stings as arguments and returns True if
+#   either string occurs anywhere in the other, and False otherwise.
+#   Hint: you might want to use the built-in str operation in.
