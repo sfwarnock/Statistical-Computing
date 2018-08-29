b120491b1efa3a046135c9cb50fd10567df26d72
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-08-29 06:01:37 -0600

    Add files via upload

--- python/chapter 04/fingExercise_4.0.py
+++ python/chapter 04/fingExercise_4.0.py
@@ -10,3 +10,9 @@ Created on Tue Aug 28 2018
 # Wrtie a function isIn that accepts two stings as arguments and returns True if
 #   either string occurs anywhere in the other, and False otherwise.
 #   Hint: you might want to use the built-in str operation in.
+
+def isIn(string1, string2):
+    if string1 in string2:
+        return True
+    if string1 not in string2:
+        return False
